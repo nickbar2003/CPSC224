f009b42a2571ebd77c80ad6e3e0fecf2a95d844d
nickbar2003 <113255916+nickbar2003@users.noreply.github.com>
2023-09-02 18:40:20 -0700

    Delete third line

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,5 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!");
         System.out.println("HELLO WORLD!!");
-        System.out.println("HELLO WORLD!!");
     }
 }
